b346b70944568bab267e27ab7afa0037974e910a
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2021-08-11 06:40:56 +0100

    commit

--- models/book.go
+++ models/book.go
@@ -6,4 +6,5 @@ type Book struct {
 	Title string `gorm:"size:256" json:"title"`
 	Description string `gorm:"type:text" json:"description"`
 	UserID	uint `gorm:"not null" json:"user_id"`
+	User User `gorm:"foreignKey;UserID;constraint:onUpdate:CASCADE,onDelete:CASCADE" json:"user"`
 }
